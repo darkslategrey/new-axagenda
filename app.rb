09009372f9365f0d7e26968cbffa906ced867c50
gf <g@g.fr>
2013-12-11 11:04:45 +0100

    assets mngmt

--- app.rb
+++ app.rb
@@ -24,51 +24,18 @@ class AxAgenda < Sinatra::Base
   register Sinatra::AssetPack
 
   assets do
-    serve '/js', :from => 'public/js'
-    serve '/css', :from => 'public/css'
+    serve '/js',        :from => 'public/js'
+    serve '/css',       :from => 'public/css'
+    serve '/image',     :from => 'public/js/feyaSoft/calendar/image'
+    serve '/resources', :from => 'public/js/extjs/resources'
 
-    # js :application, '/js/app.js', [ '/js/extjs/ext-all.js',
-    js :application, '/js/axagenda.js', [ '/js/extjs/ext-all.js',
-                                          '/js/feyaSoft/calendar/common/CONST_RUBY.js',
-                                          '/js/ext_notification.js',
-                                          '/js/feyaSoft/calendar/store/DataSource.js',
-                                          '/js/feyaSoft/calendar/util/SearchField.js',
-                                          '/js/feyaSoft/calendar/util/DatePicker.js',
-                                          '/js/feyaSoft/calendar/util/LabelField.js',
-                                          '/js/feyaSoft/calendar/multi-language/fr.js',
-                                          '/js/feyaSoft/calendar/view/BasicView.js',
-                                          '/js/feyaSoft/calendar/control/EventHandler.js',
-                                          '/js/feyaSoft/calendar/view/CalendarContainer.js',
-                                          '/js/feyaSoft/calendar/view/ResultView.js',
-                                          '/js/feyaSoft/calendar/view/Viewer.js',
-                                          '/js/feyaSoft/calendar/view/CalendarWin.js',
-                                          '/js/feyaSoft/calendar/view/MainPanel.js',
-                                          '/js/feyaSoft/calendar/view/MonthView.js',
-                                          '/js/feyaSoft/calendar/view/DayView.js',
-                                          '/js/feyaSoft/calendar/view/layout/CalendarLayout.js',
-                                          '/js/feyaSoft/calendar/view/layout/BlockMap.js',
-                                          '/js/feyaSoft/calendar/view/layout/LayoutGrid.js',
-                                          '/js/feyaSoft/calendar/view/layout/Line.js',
-                                          '/js/feyaSoft/calendar/view/layout/Block.js',
-                                          '/js/feyaSoft/calendar/view/popup/SettingPopup.js',
-                                          '/js/feyaSoft/calendar/view/popup/ExpirePopup.js',
-                                          '/js/feyaSoft/calendar/view/WestPanel.js',
-                                          '/js/feyaSoft/calendar/common/BackThread.js',
-                                          '/js/feyaSoft/calendar/common/LanManager.js',
-                                          '/js/feyaSoft/calendar/common/RepeatType.js',
-                                          '/js/feyaSoft/calendar/common/CommentTip.js',
-                                          '/js/feyaSoft/calendar/common/Mask.js',
-                                          '/js/application.js',
-                                          '/js/feyaSoft/calendar/editor/EventEditor.js',
-                                          '/js/feyaSoft/calendar/editor/CalendarEditor.js',
-                                          '/js/feyaSoft/calendar/editor/DetailEditor.js',
-                                          '/js/extjs/ext-all.js' ]
+    js :application, '/js/axagenda.js', [ 'js/extjs/ext-all.js',
+                                          'js/ext_notification.js',
+                                          'js/application.js',
+                                          'js/feyaSoft/calendar/view/BasicView.js',
+                                          '/js/feyaSoft/calendar/**/*.js' ]
 
-    # css :application, [ '/js/extjs/resources/css/ext-all.css',
-    #                     '/css/main.css',
-    #                     '/js/feyaSoft/calendar/css/calendar.css' ]
-    # serve '/css', :from => 'public'
-    # css :application, '/css/app.css', [ '/css/*.css' ]
+
     css :application, '/css/axagenda.css', [ '/css/main.css',
                                              '/js/extjs/resources/css/ext-all.css',
                                              '/js/feyaSoft/calendar/css/calendar.css' ]
--- tasks/application.rake
+++ tasks/application.rake
@@ -7,3 +7,8 @@ desc 'Clean Emacs auto-save files'
 task :clean do
   system 'find -type f -name \*~ | xargs rm'
 end
+
+desc 'Clean cached files'
+task :clean_cache do
+  system 'find public/ -type f | grep -E ".*\.[a-z0-9]{32,32}\..*" | xargs rm'
+end
